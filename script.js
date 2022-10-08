10934a90cfea57c4574cdb8afaf3cdc248f4e387
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-08 10:23:52 -0300

    alternando a marca dos simbolos colocados no jogo

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const quadradosJogo = document.querySelectorAll("[data-celula");
+const jogo = document.querySelector("[data-jogo]")
+
 
 let vezCirculoJogar;
 
@@ -6,11 +8,26 @@ const colocandoSimbolo = (celula, adicionarClasse) => {
     celula.classList.add(adicionarClasse)
 }
 
+const alternandoTurno = () => {
+    vezCirculoJogar = !vezCirculoJogar
+
+    jogo.classList.remove("circulo");
+    jogo.classList.remove("x")
+
+    if(vezCirculoJogar){
+        jogo.classList.add("circulo")
+    }else{
+        jogo.classList.add("x")
+    }
+}
+
 const clickQuadrado = (e) => {
     //colocar a marca ( x ou circulo)
     const celula = e.target;
     const adicionarClasse = vezCirculoJogar ? "circulo" :"x";
-    colocandoSimbolo(celula, adicionarClasse)
+    colocandoSimbolo(celula, adicionarClasse);
+
+    alternandoTurno();
 }
 
 for (const quadrado of quadradosJogo){
